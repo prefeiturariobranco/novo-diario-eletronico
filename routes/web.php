2b46d59e656a23fadbe34793916a12c94948f866
gabrielly.aguiar <gaby.aguiar111@gmail.com>
2021-09-02 16:14:33 -0500

    commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/home', function () {
+    return redirect('/');
+});
+
+Route::get('/visualizar/{item}', [App\Http\Controllers\HomeController::class, 'show']);
+
+Route::post('/pesquisa', [\App\Http\Controllers\AdminController::class, 'search']);
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/admin',  [\App\Http\Controllers\AdminController::class, 'index']);
+    Route::get('/admin/adicionar', [\App\Http\Controllers\AdminController::class, 'create']);
+    Route::get('/admin/edit/{item}',  [\App\Http\Controllers\AdminController::class, 'edit']);
+    Route::post('/admin/{item}',   [\App\Http\Controllers\AdminController::class, 'update']);
+    Route::post('/admin', [\App\Http\Controllers\AdminController::class, 'store']);
+    Route::delete('/admin/{item}', [\App\Http\Controllers\AdminController::class, 'delete']);
+
+    Route::get('/usuarios', 'UsuarioController@index');
+    Route::get('/usuarios/adicionar', 'UsuarioController@create');
+    Route::post('/usuarios', 'UsuarioController@store');
+    Route::delete('/usuarios/{user}', 'UsuarioController@delete');
+});
